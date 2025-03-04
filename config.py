2e4a006a30f44f9b6a9202c396336f42b085db5b
Joel <huacreenciso97@gmail.com>
2025-03-03 22:11:33 -0500

    Switch database from MySQL to SQLite and update dependencies

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ logger.configure()
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-    BD_URL: str = "mysql+pymysql://root:@localhost/books"
+    BD_URL: str = "sqlite:///./sqlite3.db"  # Cambiado a SQLite
     CORS_ORIGINS: list[str] = ["*"]
     SKIP_AUTH: bool = True
 
